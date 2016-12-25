5c2679163ad3ddc55137f1ca715b3d6c6fd0fdbb
GSGerritsen <gerrit.sage.gerritsen@gmail.com>
2016-12-24 16:16:56 -0800

    Added init function

--- todolist/read-write.go
+++ todolist/read-write.go
@@ -2,23 +2,39 @@ package main
 
 import (
 	"encoding/json"
-	//"fmt"
+	"fmt"
 	"io/ioutil"
+	"os"
 )
 
-// need to add an init
-
-func check(e error) {
+func checkError(e error) {
 	if e != nil {
 		panic(e)
 	}
 }
 
+// need to add an init: create empty TodoList struct, create a todos.json, marshal empty TodoList into it. Make it conditional
+// on whether or not a todos.json already exists. Call in the beginning of main
+
+func initialize() {
+	if _, e := os.Stat("./todos.json"); os.IsNotExist(e) {
+		var emptySlice []*Todos
+		todolist := TodoList{Todos: emptySlice}
+		j, err := json.Marshal(todolist)
+		checkError(err)
+		error := ioutil.WriteFile("./todos.json", j, 0644)
+		checkError(error)
+		fmt.Println("Directory intialized, todos.json file created.")
+		return
+	}
+	return
+}
+
 func writeToFile(todolist *TodoList) {
 	j, err := json.Marshal(todolist)
-	check(err)
+	checkError(err)
 	error := ioutil.WriteFile("./todos.json", j, 0644)
-	check(error)
+	checkError(error)
 }
 
 func readUpdatedFile() TodoList {
